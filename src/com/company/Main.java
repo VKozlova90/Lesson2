86b249d3a4ca0d8bcc0e0cb036cdfdaf9af4bfce
VKozlova90 <fancygirl90@mail.ru>
2020-10-19 21:24:32 +0300

    v1

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+   package com.company;
+
+        public class Main {
+
+            public static void main (String[] args) {
+                // write your code here
+                int yearOfBirthday = 1990;
+                int monthOfBirthday = 5;
+                int dayOfBirthday = 26;
+                int sum = yearOfBirthday + monthOfBirthday + dayOfBirthday;
+                System.out.println("Сумма моего года, месяца и дня рождения:" + " " + sum);
+
+                int monthBiggerDay = 5;
+                boolean res = monthBiggerDay > dayOfBirthday;
+                System.out.println("Месяц моего рождения больше даты рождения:" + res);
+
+                char [] myname = new char[8];
+                myname [0] = 'В';
+                myname [1] = 'и';
+                myname [2] = 'к';
+                myname [3] = 'т';
+                myname [4] = 'о';
+                myname [5] = 'р';
+                myname [6] = 'и';
+                myname [7] = 'я';
+                System.out.println(myname);
+
+                char[] myName = new char [] {'В','и','к','т','о','р','и','я'};
+                System.out.println(myName);
+
+                double myAge = 30;
+                double partYear = 4.0/12.0;
+                double res1 = myAge + partYear;
+                System.out.println(res1);
+            }
+        }
+
+
+}
+}
